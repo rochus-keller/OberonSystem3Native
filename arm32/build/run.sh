bf447c3efb24e1457619e70d80ccf3dde2247b4f
Rochus Keller <me@rochus-keller.ch>
2026-04-04 21:45:36 +0200

    mastered the next odyssey through the uart and config jungle of rpi 3b, TracePort=0,1,2 work

--- arm32/build/run.sh
+++ arm32/build/run.sh
@@ -1,8 +1,9 @@
 qemu-system-arm \
   -machine raspi2b \
   -kernel ../output/image.bin \
-  -append ";;BootVol=SYS AosFS SD0#0;AosFS=OFSDiskVolumes.New OFSAosFiles.NewFS;MT=;MP=;MB=-3;DMASize=14800H;TraceModules=1;Display=;DDriver=DisplayLinear;DMode=;TraceConsole=1;" \
+  -append ";;BootVol=SYS AosFS SD0#0;AosFS=OFSDiskVolumes.New OFSAosFiles.NewFS;MT=;MP=;MB=-3;DMASize=14800H;TraceModules=1;Display=;DDriver=DisplayLinear;DMode=;TracePort=1;" \
   -semihosting-config enable=on,target=native \
+  -serial stdio \
   -usb -device usb-kbd -device usb-mouse \
   -drive file=../output/drive.img,format=raw,if=sd
   
